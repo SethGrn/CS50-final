db1fa1e72d54fa5003fbff978482f16e6a1d217a
Seth Greene <sethandrewg@gmail.com>
2023-12-16 14:12:03 -0500

    Completed Header.js file

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function Header() {
+    return (
+        <div>
+            <div className="p-5 text-center bg-dark">
+                <div className="container py-5">
+                    <h1 className="text-light">Welcome to GigaNote</h1>
+                    <p className="col-lg-8 mx-auto lead"> </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header; 
