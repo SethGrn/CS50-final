0b059d44230e24da3a1a72e7935c9ed2ac32304c
Seth Greene <sethandrewg@gmail.com>
2023-12-16 14:09:19 -0500

    committed main project files and deletion of uneeded files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./Header"
+import Sidebar from "./Sidebar"
+import NoteView from "./NoteView"
+import NotFound from "./NotFound"
+import data from "./data/notes-data.js"
+
 import './App.css';
 
 function App() {
+   const [notes, setNotes] = useState(data);
+   const [active, setActive] = useState(notes[0]);
+
+   const addNote = () => {
+
+    const newId = notes.reduce((largestID, currentNote) => {
+        return currentNote.id > largestID ? currentNote.id : largestID;
+      }, null);
+
+      console.log("New Id when creating a note: ", newId)
+    const newNote = {
+      title: "New Note",
+      body: "",
+      id: newId + 1,
+    }
+
+    setNotes ([...notes, newNote])
+
+    setActive(newNote)
+
+   }
+
+
+   const deleteNote = (noteToDelete) => {
+    setNotes(
+      notes.filter((note) => note !== noteToDelete)
+    )
+    setActive(0)
+   }
+   
+  const setActiveNote = (note) => {
+    setActive(note)
+  }
+
+  const editNote = (noteId, name, value) => {
+    const updatedNotes = notes.map((note) => {
+      if (note.id !== noteId) return note
+      return {
+        ...note,
+        [name]: value,
+      }
+    })
+    setNotes(updatedNotes)
+  }
+
+  const findNoteIndex = (noteId) => {
+    let noteIndex = 0;
+    notes.find((note) => {
+      noteIndex++;
+      return note.id === noteId
+    })
+
+    return noteIndex
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="bg-dark-subtle">
+        <Header />
+        <div class="d-flex w-100">
+            <Sidebar notes={ notes } addNote = { addNote } setActiveNote={ setActiveNote } active={ active }/>
+            <NoteView note={ active } deleteNote={ deleteNote } editNote={editNote} findNoteIndex={findNoteIndex}/>
+        </div>
+      </div>
+      
+      <Routes>
+        <Route element={<NotFound />}/>
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
