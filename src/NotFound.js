962da9ec7d57a78f8c4016053e48331e4d179ebe
Seth Greene <sethandrewg@gmail.com>
2023-12-16 15:31:44 -0500

    Deleted unused files

--- src/NotFound.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { useLocation } from "react-router-dom"
-
-function NotFound () {
-    const { pathname } = useLocation()
-    return <h1>{`Path ${pathname} does not exist`}</h1>
-}
-
-export default NotFound
